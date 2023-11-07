7c33262edef9e870d39c23c9c13eea5e6970d92b
Víctor Posada <victorposadasoto@gmail.com>
2023-11-07 22:12:02 +0100

    Add InfluxDB task, vars and playbook

--- vault.sh
+++ vault.sh
@@ -4,9 +4,13 @@ echo "Creating ansible vault"
 echo ""
 read -p "Type your ansible vault password: " vault_password
 read -p "Type your PiHole password: " pihole_password
+read -p "Type your InfluxDB admin password: " influx_root_password
+read -p "Type your InfluxDB user password: " influx_user_password
 echo $vault_password > ~/.ansible/.secret_pi
 
 ansible-vault encrypt_string --vault-password-file ~/.ansible/.secret_pi "$pihole_password" --name "pihole_password" >> ansible/vault.yml
+ansible-vault encrypt_string --vault-password-file ~/.ansible/.secret_pi "$influx_root_password" --name "influx_root_password" >> ansible/vault.yml
+ansible-vault encrypt_string --vault-password-file ~/.ansible/.secret_pi "$influx_user_password" --name "influx_user_password" >> ansible/vault.yml
 
 echo ""
 echo "Vault file has been created!"
